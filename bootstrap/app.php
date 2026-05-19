2ac0daae19a839134b3b804756d15efaec292f66
Abhyasa Gunawan Yusuf <abiabzu0@gmail.com>
2026-05-20 02:03:57 +0700

    update webhook & setup ngrok

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/webhook/midtrans',
         ]);
+
+        // INI TAMBAHAN BARUNYA: Mempercayai HTTPS dari Ngrok
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
